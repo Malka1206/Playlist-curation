71c17e0a4a3c26978745d0fa9976da893f21b56f
Naoures Abassi <naoures.abassi@telecom-paris.fr>
2025-04-08 09:40:53 +0000

    Edit passage_par_0.py

--- Code/passage_par_0.py
+++ Code/passage_par_0.py
@@ -5,15 +5,18 @@ import matplotlib.pyplot as plt
 # Charger un fichier audio
 y, sr = librosa.load("C:\\Users\\MSI\\Desktop\\TELECOM\\Artishow\\playlist-curation\\Frequency features\\hiphop.00005.au")
 
-# Calculer le taux de passage par zéro
-zero_crossings = librosa.feature.zero_crossing_rate(y)[0]
+def compute_passage_0(y,sr):
+    # Calculer le taux de passage par zéro
+    zero_crossings = librosa.feature.zero_crossing_rate(y)[0]
 
-# Extraire les caractéristiques
-mean_zcr = np.mean(zero_crossings)
+    # Extraire les caractéristiques
+    mean_zcr = np.mean(zero_crossings)
 
-var_zcr = 0 
-for i in range(len(zero_crossings)):
-    var_zcr= (zero_crossings(i)-mean_zcr)**2
+    var_zcr = 0 
+    for i in range(len(zero_crossings)):
+        var_zcr= (zero_crossings(i)-mean_zcr)**2
+    
+    return mean_zcr, var_zcr
 
 
 # Affichage du taux de passage par zéro
