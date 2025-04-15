b7a9e193e3e4484b40b4ea671ee35665dbe71b17
Naoures Abassi <naoures.abassi@telecom-paris.fr>
2025-04-15 09:49:26 +0000

    Edit Création_vecteur.py

--- Code/Création_vecteur.py
+++ Code/Création_vecteur.py
@@ -6,6 +6,8 @@ from Flux_spectral import compute_spectral_flux
 from Rollof_spectral import compute_spectral_rollof
 from passage_par_0 import compute_passage_0
 from Caractéristique_à_faible_énergie import compute_carac_faible_energie
+from Carac_a_partir_du_BH import compute_beat_characterestics
+from Carac_a_partir_du_PH import compute_tonal_character
 
 #FenAnalyse = 0.023 s
 #FenTexture = 1 s --> 43 fenêtres d'analyse
@@ -29,6 +31,8 @@ def création_vecteur(signal,Fs, n_fft, hop_size, n_mels, n_mfcc):
     SpectFlux_var,SpectFlux_mean = compute_spectral_flux(signal,n_ftt,hop_size)
     Pass0_var,Pass0_mean = compute_passage_0(signa,FS)
     LowEnergy = compute_carac_faible_energie(signal,Fs)
+    A0, A1, RA, P1, P2, SUM = compute_beat_caracterics(signal)
+
     vect = np.concatenate((MFCC_var,MFCC_mean),axis=0)
 
 
