b696f0cf7122dfff00ed7b4a949ce62dfa0d1366
Naoures Abassi <naoures.abassi@telecom-paris.fr>
2025-05-26 12:49:02 +0000

    Edit Flux_spectral.py

--- Code/Flux_spectral.py
+++ Code/Flux_spectral.py
@@ -6,13 +6,17 @@ def charger_audio(chemin_fichier, sr=16000):
     signal, Fs = librosa.load(chemin_fichier, sr=sr)
     return signal, Fs
 
-def compute_spectral_flux(signal,n_fft, hop_length):
-    stft = np.abs(librosa.stft(signal,n_fft=n_fft,hop_length= hop_length))
-    stft_norm = stft / np.sum(stft, axis=0, keepdims=True)
+def compute_spectral_flux(signal, n_fft, hop_length): 
+    stft = np.abs(librosa.stft(signal, n_fft=n_fft, hop_length=hop_length))
+    
+    epsilon = 1e-16  # Pour éviter la division par zéro
+    stft_norm = stft / (np.sum(stft, axis=0, keepdims=True) + epsilon)
+    
     spectral_flux = np.sum((np.diff(stft_norm, axis=1))**2, axis=0)
-    spectral_flux_variances=np.var(spectral_flux)
-    spectral_flux_means=np.mean(spectral_flux)
-    return spectral_flux_variances,spectral_flux_means
+    spectral_flux_variances = np.var(spectral_flux)
+    spectral_flux_means = np.mean(spectral_flux)
+    
+    return spectral_flux_variances, spectral_flux_means
     
 
 """# Test du MFCC et charger le fichier audio
